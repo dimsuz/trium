aa251ef3fcf37c1a2d5921f215435215488c51da
Dmitry Suzdalev <dimsuz@gmail.com>
2014-04-05 02:16:31 +0400

    Start defining a left sidebar

--- src/trium/core.cljs
+++ src/trium/core.cljs
@@ -4,15 +4,39 @@
 
 (enable-console-print!)
 
+(def gui-data
+  {:left-sidebar {:items [{:title "MAIN" :type :header}
+                          {:title "Play Queue"}
+                          {:title "News"}
+                          {:title "COLLECTION" :type :header}
+                          {:title "Library"}
+                          {:title "Favorites"}
+                          {:title "History"}
+                          {:title "OTHER" :type :header}
+                          {:title "Local files"}
+                          ]}})
+
 (def app-state
   (atom
-    {:text "Hello, Trium"}))
+   {:ui gui-data}
+   {:text "Hello, Trium"}))
 
-(defn test-view [app owner]
+(defn sidebar-item [item owner]
   (reify
     om/IRender
     (render [_]
-      (dom/h2 nil (:text app)))))
+      (dom/li (when (= :header (:type item)) #js {:className "uk-nav-header"})
+              (dom/a nil (:title item))))))
 
-(om/root test-view app-state
+(defn sidebar-view [sidebar owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/ul #js {:className "uk-nav"}
+             (om/build-all sidebar-item (:items sidebar))))))
+
+(defn app-ui [app owner]
+  (sidebar-view (get-in app [:ui :left-sidebar])))
+
+(om/root app-ui app-state
          {:target (. js/document (getElementById "testview"))})
