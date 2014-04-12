2826ebb45bccb5166c1f798408c96f08750c694a
Dmitry Suzdalev <dimsuz@gmail.com>
2014-04-12 14:33:19 +0400

    add watch for css changes

--- helpers/livereload.js
+++ helpers/livereload.js
@@ -8,6 +8,10 @@ fs.watch('./main.js', [], function(event, filename) {
   if (location && /\.js$/.test(filename))
     location.reload(false);
 });
+fs.watch('./css/trium.css', [], function(event, filename) {
+  if (location && /\.css$/.test(filename))
+    location.reload(false);
+});
 fs.watch('index.html', [], function(event, filename) {
   if (location && /\.html$/.test(filename))
     location.reload(false);
