55f40c7a0b0a89db0edd7fefd3b6e3406f470da7
Dmitry Suzdalev <dimsuz@gmail.com>
2014-05-01 23:00:14 +0400

    more thinking around adding missing artists, continue toying with approaches

--- src/trium/storage.cljs
+++ src/trium/storage.cljs
@@ -1,4 +1,8 @@
-(ns trium.storage)
+(ns trium.storage
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :as async :refer [put! chan <! close!]]
+            [clojure.set :refer [rename-keys]])
+  )
 
 (def mock-data [ {:title "Es un sombrero" :album "Inocencia" :artist "Bosques de mi Mente"
                   :source "/home/dimka/Home/dimka/Music/Bosques de mi Mente/Inocencia/01 Es un sombrero.mp3"}
@@ -23,15 +27,78 @@
   (let [Datastore (js/require "nedb")]
     (Datastore.)))
 
+(def next-id (atom 0))
+(defn gen-id [] (swap! next-id inc))
+
+(defn findOne [db query-req ch]
+  (.findOne db (clj->js (:query query-req)) (fn [err doc] (put! ch {:id (:id query-req) :res doc :err err})))
+  ch)
+
+;; TODO sink with count
+(defn sink
+  "Returns an atom containing a vector. Consumes values from channel
+  ch and conj's them into the atom."
+  [ch]
+  (let [a (atom [])]
+    (go
+      (loop []
+        (let [val (<! ch)]
+          (when-not (nil? val)
+            (swap! a conj val)
+            (recur)))))
+    @a))
+
+(defn query-req [query]
+  (hash-map :id (gen-id) :query query))
+
 (defn artists-of [db tracks]
-  (let [artists (into #{} (map :artist tracks))]
-    (println artists))
-  ;(.findOne db #js {:name (:artist t)} (fn [err doc] (put!)))
+  (let [queries (map hash-map (repeat :name) (distinct (map :artist tracks)))
+        query-reqs (map query-req queries)
+        ch (chan)
+        outch (chan)
+        ]
+    (println query-reqs)
+    ;; run all queries and close the channel
+    (go
+      (doseq [req query-reqs]
+        (let [res (<! (findOne db req ch))]
+          (put! outch (if-let [artist (:res res)] artist :no-artist))))
+      (close! ch)
+      (close! outch))
+    outch))
+
+(defn resolve-artist-id [artist]
+  "given an {:name '...'} map adds an :id key to it with DB id or nil if missing in DB.
+Returns a channel from which a resulting map can be read on completion"
+
   )
 
-(defn insert-tracks! [db tracks]
-  (let [a_ids (artists-of db tracks)])
+(defn add-missing-artists [artists]
+  (let [c (chan)]
+    )
   )
+
+(defn get-artists [tracks]
+  (distinct (map (fn [t]
+                   (-> t
+                       (select-keys [:artist])
+                       (rename-keys {:artist :name})))
+                 tracks)))
+
+(defn insert-tracks! [db tracks]
+  ;; goal => loop each distinct artist filter out those which are missing, add them, then proceed with adding
+  ;; all tracks, artists will exist 100%
+  ;; transform tracks array to array of distinct artist names {:name } {:name }
+  ;; inside add-missing-artists:
+  ;; doseq resolve-artist-id => chan => [{:name '...' :id '...'} {:name '...' :id :no-id}] => (map< chan) => chan [{:name '...' :id :no-id} {:no-id}] =>
+
+  (let [artists get-artists]
+    (go
+      ;; reading from chanel here is a noop => just wait until adding is complete
+      (<! (add-missing-artists artists))
+      ;; TODO actual insert
+      )
+))
 
 (defn create-and-fill-database []
   (let [db (create-database)
