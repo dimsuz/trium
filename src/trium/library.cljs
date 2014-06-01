0a6dd3f1d747b97362061a778e52aa30a12bee8c
Dmitry Suzdalev <dimsuz@gmail.com>
2014-06-02 00:40:15 +0400

    Play with library view

--- src/trium/library.cljs
+++ src/trium/library.cljs
@@ -9,18 +9,19 @@
   (reify
     om/IRender
     (render [_]
-      (dom/div nil
-               (str (:name album) " by "
-                    (:artist album))
-               )
-      )
-    ))
+      (dom/div #js {:className "uk-width-1-3 album-component"}
+               (dom/a #js {:className "uk-thumbnail uk-thumbnail-medium"}
+                      (dom/img #js {:src "images/placeholder_600x400.svg" :alt ""})
+                      (dom/div #js {:className "uk-thumbnail-caption"}
+                               (dom/div nil (:artist album))
+                               (dom/div nil (:name album))))))))
 
 (defn albums-component [app owner]
   (reify
     om/IRender
     (render [_]
-      (apply dom/div nil (om/build-all album-component (sort-by :artist (:query-result app))))
+      (apply dom/div #js {:className "uk-grid"}
+             (om/build-all album-component (sort-by :artist (:query-result app))))
       )
     )
   )
