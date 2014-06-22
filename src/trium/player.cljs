8efa0b2db5838fe143d9672716138a289afce310
Dmitry Suzdalev <dimsuz@gmail.com>
2014-06-23 01:08:41 +0400

    Move playing state changes into player/ functions

--- src/trium/player.cljs
+++ src/trium/player.cljs
@@ -1,35 +1,50 @@
 (ns trium.player
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :refer [put! chan <! timeout]]))
-
-(declare handleLoad)
+  (:require
+   [om.core :as om :include-macros true]
+   [cljs.core.async :refer [put! chan <! timeout]]))
 
 (def Sound (.-Sound js/createjs))
 
 (defn init []
-  (.addEventListener Sound "fileload" handleLoad))
+  )
 
-(defn handleLoad [e]
-  (prn "handled load" e))
+(defn sound-id [track] (:source track))
 
-(defn play [track comm]
-  (go
-    ;(<! (timeout 1000))
-    (.registerSound Sound (str "file://" (:source track)) "sound_id")
-    (.play Sound "sound_id")
-    (put! comm [:playing (assoc track :duration "02:55")]))
-  comm)
+(defn handleLoad [app track e]
+  (.removeAllEventListeners Sound "fileload")
+  (println (str "ready to play " (:title track) ", starting playback"))
+  (let [instance (.play Sound (sound-id track))
+        playing? (= (.-PLAY_SUCCEEDED Sound) (.-playState instance))]
+    (println (str (if playing? "Started playing " "Failed to play ")) track)
+    (om/transact! app
+                  (fn [app]
+                    (if playing?
+                      (assoc app :current-track track :player-state :playing)
+                      (assoc app :current-track nil :player-state :paused))))))
+
+(defn stop [app]
+  (println "Stopping current track")
+  (.stop Sound)
+  (om/transact! app (fn [app] (assoc app :current-track nil :player-state :paused))))
+
+(defn play [app track]
+  (.addEventListener Sound "fileload" (partial handleLoad app track))
+  (let [res (.registerSound Sound (str "file://" (:source track)) (sound-id track))]
+    (if (= true res)
+      (do
+        ;; TODO Prettify! document!
+        (println "removing")
+        (.removeAllSounds Sound)
+        (.registerSound Sound (str "file://" (:source track)) (sound-id track))
+        (println "registered sound II" res))
+      (println "registered sound" res)))
+  ;; sound will start playing in handleLoad, after loading complete
+)
 
 (defn pause [comm]
   (go
     ;(<! (timeout 1000))
     (.stop Sound)
     (put! comm [:paused]))
-  comm)
-
-(defn stop [comm]
-  (go
-    ;(<! (timeout 1000))
-    (.stop Sound)
-    (put! comm [:stopped]))
   comm)
--- src/trium/queue.cljs
+++ src/trium/queue.cljs
@@ -11,19 +11,6 @@
   "Replaces queue contents with specified tracks and optionally starts playback"
   (om/update! app [:queue :tracks] tracks)
   (when play
-    (go
-      ;; first stop what is playing.
-      ;; TODO correctly update app state! because if loading or playing then fails, app
-      ;; needs to be in not-playing state
-      (<! (player/stop (chan)))
-      ;; TODO move this (and stopping) inside player/play and player/stop.
-      ;; let it use Sound.js or whatever it uses internally and let it update
-      ;; app state there too
-      (let [[state track] (<! (player/play (first tracks) (chan)))]
-        (om/transact! app
-                      (fn [app]
-                        (if (= :playing state)
-                          (assoc app :current-track track :player-state :playing)
-                          (assoc app :current-track nil))))))
-    )
-  )
+    ;; first stop what is playing.
+    (player/stop app)
+    (player/play app (first tracks))))
